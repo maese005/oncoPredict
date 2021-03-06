0f07d05f306b757012add1c805117244d87b60d6
maese005 <62571435+maese005@users.noreply.github.com>
2021-03-06 13:34:25 -0600

    Update IDWAS.R

--- R/IDWAS.R
+++ R/IDWAS.R
@@ -100,6 +100,25 @@ map_cnv<-function(theRootDir, Cnvs){
 #'@import TxDb.Hsapiens.UCSC.hg19.knownGene
 #'@import GenomicFeatures
 #'@export
+#'This function will test every drug against every CNV or somatic mutation for your cancer type.
+#'@param drug_prediction The drug prediction data. Must be a data frame.
+#'If you have cnv data, make sure rownames() are samples, and colnames() are drugs.
+#'If you have mutation data, make sure colnames() are samples and rownames() are drugs.
+#'Make sure sample names are of the same form as the sample names in your cnv or mutation data.
+#'e.g. if the rownames() are TCGA barcodes of the form TCGA-##-####-###, make sure your cnv/mutation data also uses samples in the form TCGA-##-####-###
+#'@param data The cnv or mutation data. Must be a data frame. colnames() are samples.
+#'If you wish to use cnv data, use the output from map_cnv(), transpose it so that colnames() are samples.
+#'If you wish to use mutation data, use the method for downloading mutation data outlined in the vignette, and
+#'make sure the TCGA barcodes use '-' instead of '.'; if not, ensure your data file includes the following columns: 'Variant_Classification', 'Hugo_Symbol', 'Tumor_Sample_Barcode'. If
+#'@param n The number of samples you want CNVs or mutations to be amplified in. The default is 10.
+#'@param cnv TRUE or FALSE. Indicate whether or not you would like to test cnv data. If TRUE, you will test cnv data. If FALSE, you will test mutation data.
+#'@keywords Test CNV or mutation data to genes.
+#'@import org.Hs.eg.db
+#'@import gdata
+#'@import parallel
+#'@import TxDb.Hsapiens.UCSC.hg19.knownGene
+#'@import GenomicFeatures
+#'@export
 test<-function(drug_prediction, data, n=10, cnv){
 
   #Check parameters.
@@ -253,6 +272,10 @@ test<-function(drug_prediction, data, n=10, cnv){
       sampIds01a <- sapply(strsplit(all01ASamples, "-", fixed=T), function(l)return(l[3])) #The TCGA #### digit number.
       colnames(preds01a) <- sampIds01a
       inPredAndMutData <- sampIds01a[sampIds01a %in% mutIds] #Samples for which we have both predicted drug response and mutation calls
+
+      if (length(inPredAndMutData) == 0){
+        stop((paste("\nERROR: Samples in drug_prediction and data don't overlap. Make sure their sample identifiers are of similar form")))
+      }
 
       #Run the associations between all genes and drugs, for drugs with at least 50 mutations.
       #_______________________________________
@@ -421,9 +444,13 @@ test<-function(drug_prediction, data, n=10, cnv){
       drug_samps<-colnames(drug_prediction)
       inPredAndMutData <- drug_samps[drug_samps %in% unique] #Samples for which we have both predicted drug response and mutation calls
 
+      if (length(inPredAndMutData) == 0){
+        stop((paste("\nERROR: Samples in drug_prediction and data don't overlap. Make sure their sample identifiers are of similar form")))
+      }
+
       #Run the associations between all genes and drugs, for drugs with at least 50 mutations.
       #_______________________________________
-      drug_prediction_filt_ord <- drug_prediction[, inPredAndMutData] #The preds for the 01A samples we have both prediction and mutation data for.
+      drug_prediction_filt_ord <- as.matrix(drug_prediction[, inPredAndMutData]) #The preds for the 01A samples we have both prediction and mutation data for.
       mutMat_nodups_ordFilt <- mutMat[, inPredAndMutData]
       commonMuts <- apply(mutMat_nodups_ordFilt, 1, sum)
       if (length(which(commonMuts >= n)) == 0){
@@ -475,7 +502,7 @@ test<-function(drug_prediction, data, n=10, cnv){
 
       output1<-sort(unlist(betaValList))[1:30]
 
-      write.table(output, file='./MutationTestOutput_pVal.txt')
+      write.table(output, file='./MutationTestOutput_pVal555555.txt')
 
       write.table(output1, file='./MutationTestOutput_betaVal.txt')
 

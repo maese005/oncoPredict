2901e8ed40422c0411ccf0ba7bf169f651a9301c
maese005 <62571435+maese005@users.noreply.github.com>
2021-03-30 11:17:54 -0500

    Update IDWAS.R

--- R/IDWAS.R
+++ R/IDWAS.R
@@ -1,8 +1,9 @@
 #'This function maps cnv data to genes.
 #'The output of this function is a .RData file called map.RData; this file contains theCnvQuantVecList_mat (rows are genes, and columns are samples) and tumorSamps (indicates which samples are primary tumor samples, 01A).
-#'@param Cnvs The output obtained from the download() function (the cnv data); a table with colnames() Sample (named using the TCGA patient barcode), Chromosome, Start, End, Num_Probes, and Segment_Mean.
+#'@param Cnvs The cnv data. A table with the following colnames: Sample (named using the TCGA patient barcode), Chromosome, Start, End, Num_Probes, and Segment_Mean.
 #'@keywords Map CNV data to genes
 #'@return A .RData file called, map.RData, which stores two objects: theCnvQuantVecList_mat (rows are genes, columns are samples), tumorSamps (indicates which samples are primary tumor/01A).
+#'This output will serve as the input for test(). 
 #'@import org.Hs.eg.db
 #'@import GenomicFeatures
 #'@export
